a37c8ab0bcdd98019efd69c970f22a337a21c32e
IGBC <fakespamtrap@gmail.com>
2016-06-17 23:13:05 +0200

    stuff

--- db_displayd/deamon.py
+++ db_displayd/deamon.py
@@ -1,43 +1,60 @@
 #!/usr/bin/env python3
-from config import LEAFS
-from socket import Socket
-from serial import Serial
+# from config.py import LEAFS
+import socket
+import time
+from serial import Serial, time
 import paho.mqtt.client as mqtt
+import subprocess
 
-client
-sock
+client = None
+sock = None
+
 
 def setup():
+    print("Starting db_displayd")
     global client
-    client = mqtt.Client(client_id="piui1")
+    client = mqtt.Client(client_id="piui_c-base")
     client.on_connect = on_connect
     client.on_message = on_message
+    client.connect("msggwt1.service.deutschebahn.com", 1905, 60)
+    print("Connected to MQTT")
     global sock
-    sock = # todo
+    sock = None  # todo
+
 
 
 def main():
-    '''Main Deamon loop'''
-	
+    time.sleep(1000)
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    #client.subscribe("$PIUI/#")
+    # client.subscribe("$PIUI/#")
     client.subscribe("$PIUI/display/#")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload)) # decode this
-    output = subprocess.check_output(msg.payload) 
-    # client.publish("$PIUI/out", payload=output, qos=0, retain=False)
-	
+    print(msg.topic + " " + str(msg.payload))
+    # decode this
+
+
 def list():
+    pass
+
 
 def switch(drum, leaf):
+    pass
+
 
 def reset(drum):
+    pass
 
+
+if __name__ == "__main__":
+    setup()
+    client.loop_forever()
